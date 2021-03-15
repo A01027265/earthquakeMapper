71c8408fd2d6aba4993c262aedeb3f225a7de86e
empobla <A01027265@itesm.mx>
2021-03-14 18:52:33 -0600

    updated limit restriction

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.set('trust proxy', 1);  // Reverse proxy (Heroku)
 const limiter = rateLimit({
   windowMS: 1000 * 60 * 10,  // 10 min in ms
   max: 50, // Max requests per 10 min window
-  message: 'You have exceeded the 100 requests in 15 minutes limit!',
+  message: 'You have exceeded the 50 requests in 10 minutes limit!',
   headers: true // Send the appropriate headers to the response (X-RateLimit-Limit, X-RateLimit-Remaining, Retry-After)
 });
 
